5ca2554ccb01404ae38c798db07db90147787578
JakeStewart7 <jake.stewart.us@gmail.com>
2019-12-03 16:34:31 -0800

    Trebuchet Launching
    Changed RotateArm script so you can press W to start the launch process for the trebuchet. Added a sphere that stays with the sling until the arm rotates a certain amount, then it detaches and flies tangent to sling (sling's forward direction). Added gravity by constantly updating its direction based on gravity constant.

--- 4x4GraphicsProject/Assets/Scripts/NodeActions/Rotation/RotateArm.cs
+++ 4x4GraphicsProject/Assets/Scripts/NodeActions/Rotation/RotateArm.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class RotateArm : MonoBehaviour
 {
+    enum State {Stationary, Launching, Resetting};  // States of the trebuchet
+    private State currentState = State.Stationary;
+
     public SceneNode Arm;
+    public SceneNode Sling;
+
+    private float end_time = 0;  // Keeps track of when the trebuchet can stop launching or resetting
+    private float fire_time = 1f;  // Time spent in "Launching" or "Resetting" state
     float deltaRotation = 180f;  // Degrees per second
+    float slingRotationFactor = 0.9f; // Percent of deltaRotation used for the sling's rotation
 
     void Start()
     {
@@ -14,27 +22,62 @@ public class RotateArm : MonoBehaviour
 
     void Update()
     {
-        // While holding down the "a" key, rotate left
-        if (Input.GetKey("w"))
+        switch(currentState)
         {
-            rotateForward();
+            case State.Stationary:
+                stationaryAction();
+                break;
+            case State.Launching:
+                launchingAction();
+                break;
+            case State.Resetting:
+                resettingAction();
+                break;
+            default:
+                break;
         }
-        // While holding down the "d" key, rotate left
-        if (Input.GetKey("s"))
+    }
+    
+    // Trebuchet is stationary until "w" is pressed
+    // This sets its state to "Launching" and it starts a launching timer
+    void stationaryAction()
+    {
+        // Once the "w" key is pressed, the trebuchet starts launching
+        if (Input.GetKeyDown("w"))
         {
-            rotateBackward();
+            currentState = State.Launching;
+            end_time = Time.realtimeSinceStartup + fire_time;
         }
     }
 
-    // Rotate base to the right by negative deltaRotation
-    void rotateForward()
+    // Until the end_time is reached, the arm is rotated forward
+    // Once the time is up, it resets the timer and sets its state to "Resetting"
+    void launchingAction()
     {
-        Arm.transform.Rotate(-deltaRotation * Time.deltaTime, 0, 0);
+        if (end_time > Time.realtimeSinceStartup)
+        {
+            Arm.transform.Rotate(-deltaRotation * Time.deltaTime, 0, 0);
+            Sling.transform.Rotate(-deltaRotation * slingRotationFactor * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            currentState = State.Resetting;
+            end_time = Time.realtimeSinceStartup + fire_time;
+        }
     }
 
-    // Rotate base to the left by deltaRotation
-    void rotateBackward()
+    // Does the same thing as launchingAction, but in reverse to reset its position
+    // Once the time is up, it sets its state back to "Stationary"
+    void resettingAction()
     {
-        Arm.transform.Rotate(deltaRotation * Time.deltaTime, 0, 0);
+        if (end_time > Time.realtimeSinceStartup)
+        {
+            Arm.transform.Rotate(deltaRotation * Time.deltaTime, 0, 0);
+            Sling.transform.Rotate(deltaRotation * slingRotationFactor * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            currentState = State.Stationary;
+        }
     }
 }
---
+++ 4x4GraphicsProject/Assets/Scripts/ProjectileAction/SphereAction.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereAction : MonoBehaviour
+{
+    enum State {Attached, Detached};  // States of the sphere
+    private State currentState = State.Attached;
+
+    public SceneNode Base;
+    public SceneNode Arm;
+    public SceneNode Sling;
+
+    public SceneNode[] hierarchy;
+
+    private Vector3 launchDir;  // Direction of projectile being launched
+    private float launchSpeed = 150f;   // Arbitrary launch speed
+    private float gravity = -9.81f;     // Earth's gravity in meter's per second squared
+    private float gravityFactor = 0.1f;  // Make gravity realistic
+
+    void Start()
+    {
+        hierarchy = new SceneNode[] {Base, Arm, Sling};  // Used to calculate projectile position while attached to sling
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case State.Attached:
+                attachedAction();
+                break;
+            case State.Detached:
+                detachedAction();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Keeps the projectile attached to the end of the sling
+    void attachedAction()
+    {
+        // WARNING: MY MP4 SOLUTION TO CALCULATE THE ENDPOINT OF THE LAST NODE
+        transform.position = new Vector3(0, 0, 0);
+        transform.localRotation = Quaternion.identity;
+
+        Vector3 nodeScale = new Vector3(1, 1, 1);
+        Quaternion nodeRotation = Quaternion.identity;
+        Vector3 nodeTranslation = new Vector3(0, 0, 0);
+
+        for(int i = 0; i < 3; i++)
+        {
+            Vector3 nodePosition = hierarchy[i].transform.localPosition;
+            Vector3 nodeOrigin = hierarchy[i].NodeOrigin;
+
+            nodeRotation *= hierarchy[i].transform.localRotation;
+            nodeTranslation = hierarchy[i].transform.position;
+            nodeScale = Vector3.Scale(nodeScale, hierarchy[i].transform.localScale);
+
+            transform.position += transform.rotation * Vector3.Scale(nodeOrigin, nodeScale);
+            transform.rotation = nodeRotation;
+        }
+        
+        Vector3 baseScale = Base.transform.localScale;
+        Vector3 armScale = Arm.transform.localScale;
+        
+        transform.position += nodeTranslation;
+        transform.position += -Sling.PrimitiveList[0].transform.up * Sling.PrimitiveList[0].transform.localScale.y * 2;
+
+        transform.position = Vector3.Scale(transform.position, baseScale);
+        transform.position = Vector3.Scale(transform.position, armScale);
+
+        // COULD NOT FIGURE OUT HOW TO GET TRANSFORM LOCALROTATION CORRECTLY
+        // Once arm is approximately vertical, launch the ball tangent to the sling (sling's forward direction)
+        if (Arm.transform.rotation.x < -0.8)
+        {
+            currentState = State.Detached;
+            launchDir = Sling.PrimitiveList[0].transform.forward;
+        }
+        
+    }
+
+    void detachedAction()
+    {
+        // Move the ball based on launch speed and direction
+        // Direction is constantly changing based on gravity to make it realistic
+        transform.position += launchSpeed * launchDir * Time.deltaTime;
+        launchDir = ((launchSpeed * launchDir) + (gravity * gravityFactor * Vector3.up)).normalized;
+    }
+}
